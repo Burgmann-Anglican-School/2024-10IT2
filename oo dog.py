f71a3822647cdd4123d09f23043d69a2e89f4870
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2024-09-16 14:30:53 +1000

    initial file upload

---
+++ oo dog.py
@@ -0,0 +1,10 @@
+class Dog:
+
+    def __init__(self, health, happiness, hunger, cleanliness, name):
+        self.health = health
+        self.happiness = happiness
+        self.hunger = hunger
+        self.cleanliness = cleanliness
+        self.name = name
+
+dog1 = Dog(5,6,8,7,'Coco')
---
+++ procedural dog.py
@@ -0,0 +1,8 @@
+dog_health = 10
+dog_happiness = 10
+dog_hunger = 10 #this number goes down. 10 is full.
+dog_cleanliness = 10
+dog_name = 'Coco'
+
+dog1 = {'health': 10, 'happiness': 10, 'hunger': 10, 'cleanliness': 10, 'name': 'Coco'}
+dog2 = {'health': 10, 'happiness': 10, 'hunger': 10, 'cleanliness': 10, 'name': 'Doorg'}
